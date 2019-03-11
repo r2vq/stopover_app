46750d0b78fbdb1cdf8695a9c248f471ca8c5a5a
Denesh Raymond <draymond@tribalscale.com>
2019-03-11 17:29:34 -0400

    Separate fetching of data into api interface.
    Co-authored-by: Keane Quibilan <kquibilan@tribalscale.com>

--- lib/repository/stop_over_repository.dart
+++ lib/repository/stop_over_repository.dart
@@ -1,39 +1,18 @@
+import 'package:stopover_app/api/stop_over_api.dart';
 import 'package:stopover_app/model/category.dart';
 import 'package:stopover_app/model/poi.dart';
 
 class StopOverRepository {
+
+  final StopOverApi _api;
+
+  StopOverRepository(this._api);
+
   Future<List<Category>> fetchCategories() async {
-    return [
-      Category("1", "Shops",
-          "http://fluttercrashcourse.com/assets/images/fuji@3x.jpg"),
-      Category("2", "Services",
-          "http://fluttercrashcourse.com/assets/images/arashiyama@3x.jpg"),
-      Category("3", "Food",
-          "http://fluttercrashcourse.com/assets/images/kiyomizu-dera@3x.png"),
-    ];
+    return _api.getCategories();
   }
 
   Future<List<Poi>> fetchPois(String categoryId) async {
-    String url;
-
-    switch (categoryId) {
-      case "1":
-        url = "http://fluttercrashcourse.com/assets/images/fuji@3x.jpg";
-        break;
-      case "2":
-        url = "http://fluttercrashcourse.com/assets/images/arashiyama@3x.jpg";
-        break;
-      case "3":
-      default:
-        url =
-            "http://fluttercrashcourse.com/assets/images/kiyomizu-dera@3x.png";
-        break;
-    }
-
-    return [
-      Poi("Tim Hortons", url, "Something about Tim Hortons"),
-      Poi("KFC", url, "Something about KFC"),
-      Poi("Wendy's", url, "Something about Wendy's"),
-    ];
+    return _api.getPois(categoryId);
   }
 }
