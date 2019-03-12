74e1915354310ff87c9322b21003fe695a5ad2e4
Alvin Fong <aktfong@gmail.com>
2019-03-12 09:54:06 -0400

    [AF/LA] Add pois function for the pois endpoint.

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
 const functions = require('firebase-functions');
 const Firestore = require('@google-cloud/firestore');
 const PROJECTID = 'stopover-app-api';
-const COLLECTION_NAME = 'categories';
+const COLLECTION_CATEGORIES_NAME = 'categories';
+const COLLECTION_POIS_NAME = 'pois';
 
 const firestore = new Firestore({
   projectId: PROJECTID,
@@ -15,7 +16,7 @@ exports.categories = functions.https.onRequest( (request, response) => {
       if (!(id && id.length)) {
         return response.status(404).send({ error: 'Empty Id' });
       }
-      return firestore.collection(COLLECTION_NAME)
+      return firestore.collection(COLLECTION_CATEGORIES_NAME)
         .doc(id)
         .get()
         .then(doc => {
@@ -33,8 +34,28 @@ exports.categories = functions.https.onRequest( (request, response) => {
       });
 
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+exports.pois = functions.https.onRequest( (request, response) => {
+
+      const id = "QZdanoOQ2cSCZZvixeGU";
+      if (!(id && id.length)) {
+        return response.status(404).send({ error: 'Empty Id' });
+      }
+      return firestore.collection(COLLECTION_POIS_NAME)
+        .doc(id)
+        .get()
+        .then(doc => {
+          if (!(doc && doc.exists)) {
+            return response.status(404).send({ error: 'Unable to find the document' });
+          }
+          var res = [];
+          const data = doc.data();
+          res.push(data);
+          return response.status(200).send(res);
+        }).catch(err => {
+          console.error(err);
+          return response.status(404).send({ error: 'Unable to retrieve the document' });
+        });
+      });
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
