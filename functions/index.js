2fb2064e7315d26a381d754a9cb24eb034085835
Alvin Fong <aktfong@gmail.com>
2019-03-12 10:11:02 -0400

    code formatting

--- functions/index.js
+++ functions/index.js
@@ -5,58 +5,76 @@ const COLLECTION_CATEGORIES_NAME = 'categories';
 const COLLECTION_POIS_NAME = 'pois';
 
 const firestore = new Firestore({
-  projectId: PROJECTID,
-  timestampsInSnapshots: true,
+    projectId: PROJECTID,
+    timestampsInSnapshots: true,
 });
 
-exports.categories = functions.https.onRequest( (request, response) => {
-    // read/retrieve an existing document by id
-  
-      const id = "DXIQXmuFxAyiW1cYUfH3";
-      if (!(id && id.length)) {
-        return response.status(404).send({ error: 'Empty Id' });
-      }
-      return firestore.collection(COLLECTION_CATEGORIES_NAME)
-        .doc(id)
+exports.categories = functions.https.onRequest((request, response) => {
+    return firestore.collection(COLLECTION_CATEGORIES_NAME)
         .get()
-        .then(doc => {
-          if (!(doc && doc.exists)) {
-            return response.status(404).send({ error: 'Unable to find the document' });
-          }
-          var res = [];
-          const data = doc.data();
-          res.push(data);
-          return response.status(200).send(res);
+        .then(docs => {
+            var res = [];
+            docs.forEach(doc => {
+                const data = doc.data();
+                res.push(data);
+            });
+
+            return response.status(200).send(res);
         }).catch(err => {
-          console.error(err);
-          return response.status(404).send({ error: 'Unable to retrieve the document' });
+            console.error(err);
+            return response.status(404).send({
+                error: 'Unable to retrieve the document'
+            });
         });
-      });
-
-
-exports.pois = functions.https.onRequest( (request, response) => {
-
-      const id = "QZdanoOQ2cSCZZvixeGU";
-      if (!(id && id.length)) {
-        return response.status(404).send({ error: 'Empty Id' });
-      }
-      return firestore.collection(COLLECTION_POIS_NAME)
-        .doc(id)
-        .get()
-        .then(doc => {
-          if (!(doc && doc.exists)) {
-            return response.status(404).send({ error: 'Unable to find the document' });
-          }
-          var res = [];
-          const data = doc.data();
-          res.push(data);
-          return response.status(200).send(res);
-        }).catch(err => {
-          console.error(err);
-          return response.status(404).send({ error: 'Unable to retrieve the document' });
-        });
-      });
+});
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+    response.send("Hello from Firebase!");
+});
+
+exports.pois = functions.https.onRequest((request, response) => {
+    return firestore.collection(COLLECTION_POIS_NAME)
+        .get()
+        .then(docs => {
+            var res = [];
+            docs.forEach(doc => {
+                const data = doc.data();
+                res.push(data);
+            });
+            return response.status(200).send(res);
+        }).catch(err => {
+            console.error(err);
+            return response.status(404).send({
+                error: 'Unable to retrieve the document'
+            });
+        });
+});
+
+exports.poisById = functions.https.onRequest((request, response) => {
+
+    const id = "QZdanoOQ2cSCZZvixeGU";
+    if (!(id && id.length)) {
+        return response.status(404).send({
+            error: 'Empty Id'
+        });
+    }
+    return firestore.collection(COLLECTION_POIS_NAME)
+        .doc(id)
+        .get()
+        .then(doc => {
+            if (!(doc && doc.exists)) {
+                return response.status(404).send({
+                    error: 'Unable to find the document'
+                });
+            }
+            var res = [];
+            const data = doc.data();
+            res.push(data);
+            return response.status(200).send(res);
+        }).catch(err => {
+            console.error(err);
+            return response.status(404).send({
+                error: 'Unable to retrieve the document'
+            });
+        });
 });
