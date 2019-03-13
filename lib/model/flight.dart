92c24f70310eb880dcc3d2e26ed9ef867ee36b6e
Lalit <agarwal.lalit91@gmail.com>
2019-03-13 09:23:15 -0400

    LA/DR Update Flight model to contain departure and arrival info

--- lib/model/flight.dart
+++ lib/model/flight.dart
@@ -1,28 +1,59 @@
 class Flight {
   final String _flightNo;
-  final String _startAirport;
-  final String _destinationAirport;
-  final String _terminal;
-  final String _gate;
+  final String _flightName;
+  final String _flightImage;
+  final String _arrivalAirport;
+  final String _departureAirport;
+  final String _arrivalTerminal;
+  final String _departureTerminal;
+  final String _arrivalGate;
+  final String _departureGate;
+  final String _departureTime;
+  final String _arrivalTime;
+  final String _boardingTime;
 
-  Flight(this._flightNo, this._startAirport, this._destinationAirport,
-      this._terminal, this._gate);
 
-  String get gate => _gate;
-
-  String get terminal => _terminal;
-
-  String get destinationAirport => _destinationAirport;
-
-  String get startAirport => _startAirport;
+  Flight(this._flightNo, this._flightName, this._flightImage,
+      this._arrivalAirport, this._departureAirport, this._arrivalTerminal,
+      this._departureTerminal, this._arrivalGate, this._departureGate,
+      this._departureTime, this._arrivalTime, this._boardingTime);
 
   String get flightNo => _flightNo;
 
   factory Flight.fromJson(Map<String, dynamic> json) => Flight(
     json['flight_no'] as String,
-    json['start_airport'] as String,
-    json['destination_airport'] as String,
-    json['terminal'] as String,
-    json['gate'] as String
+    json['flight_name'] as String,
+    json['flight_image'] as String,
+    json['arrival_airport'] as String,
+    json['departure_airport'] as String,
+    json['arrival_terminal'] as String,
+    json['departure_terminal'] as String,
+    json['arrival_gate'] as String,
+    json['departure_gate'] as String,
+    json['arrival_time'] as String,
+    json['departure_time'] as String,
+    json['boarding_time'] as String,
   );
+
+  String get flightName => _flightName;
+
+  String get flightImage => _flightImage;
+
+  String get arrivalAirport => _arrivalAirport;
+
+  String get departureAirport => _departureAirport;
+
+  String get arrivalTerminal => _arrivalTerminal;
+
+  String get departureTerminal => _departureTerminal;
+
+  String get arrivalGate => _arrivalGate;
+
+  String get departureGate => _departureGate;
+
+  String get boardingTime => _boardingTime;
+
+  String get arrivalTime => _arrivalTime;
+
+  String get departureTime => _departureTime;
 }
--- test/unit/flight_test.dart
+++ test/unit/flight_test.dart
@@ -7,14 +7,20 @@ void main() {
 
   test('should convert from json string to flight info', () {
     const JSON_STRING =
-        '{"terminal":"3","boarding_time":"1552752000","departure_time":"1552755600","start_airport":"YYZ","flight_no":"EK 456","gate":"D30","destination_airport":"DXB"}';
-
+        '{"departure_terminal":"3","boarding_time":"2 hours","arrival_terminal":"5","flight_no":"EK 456","departure_airport":"YYZ","flight_name":"Emirates Airlines","flight_image":"https://firebasestorage.googleapis.com/v0/b/stopover-app-api.appspot.com/o/emirates_flight.jpeg?alt=media&token=118a229f-bcb0-4b95-9958-888a35370c7a","departure_time":"02:00 PM","arrival_gate":"E24","arrival_time":"10:00 AM","arrival_airport":"DXB","departure_gate":"D30"}';
     Flight flightInfo = Flight.fromJson(json.decode(JSON_STRING));
 
     expect("EK 456", flightInfo.flightNo);
-    expect("3", flightInfo.terminal);
-    expect("D30", flightInfo.gate);
-    expect("YYZ", flightInfo.startAirport);
-    expect("DXB", flightInfo.destinationAirport);
+    expect("Emirates Airlines", flightInfo.flightName);
+    expect("https://firebasestorage.googleapis.com/v0/b/stopover-app-api.appspot.com/o/emirates_flight.jpeg?alt=media&token=118a229f-bcb0-4b95-9958-888a35370c7a", flightInfo.flightImage);
+    expect("5", flightInfo.arrivalTerminal);
+    expect("3", flightInfo.departureTerminal);
+    expect("E24", flightInfo.arrivalGate);
+    expect("D30", flightInfo.departureGate);
+    expect("DXB", flightInfo.arrivalAirport);
+    expect("YYZ", flightInfo.departureAirport);
+    expect("02:00 PM", flightInfo.arrivalTime);
+    expect("10:00 AM", flightInfo.departureTime);
+    expect("2 hours", flightInfo.boardingTime);
   });
 }
