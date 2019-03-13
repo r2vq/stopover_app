cde3d84ea7b345bbcefd63b9f41c94ff0a2e3244
Lalit <agarwal.lalit91@gmail.com>
2019-03-13 13:58:59 -0400

    DR/LA Create bottom navigation tabs to display flight info and category list

--- lib/route/homepage.dart
+++ lib/route/homepage.dart
@@ -1,16 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:stopover_app/route/category_list_page.dart';
+import 'package:stopover_app/route/flight_page.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+
+  int _selectedIndex = 1;
+  final List<Widget> _children = [
+    CategoryListPage(),
+    CategoryListPage(),
+    FlightPage(),
+  ];
+
+  final List<Widget> _titles = [
+    Text("My List"),
+    Text("Deals"),
+    Text("My Flight"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Category Screen"),
+        title: _titles[_selectedIndex],
       ),
       body: Center(
-        child: CategoryListPage(),
+        child: _children[_selectedIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.star), title: _titles[0]),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.attach_money), title: _titles[1]),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.flight_takeoff), title: _titles[2]),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        fixedColor: Colors.red,
       ),
     );
   }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
 }
