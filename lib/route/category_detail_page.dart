f8327bb72645c1049f2eec6e51f31ebcf958f25d
Lalit <agarwal.lalit91@gmail.com>
2019-03-14 15:55:47 -0400

    LA/DR Change app bar style for all pages

--- lib/route/category_detail_page.dart
+++ lib/route/category_detail_page.dart
@@ -2,13 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:stopover_app/bloc/poi_bloc.dart';
 import 'package:stopover_app/model/category.dart';
 import 'package:stopover_app/model/poi.dart';
+import 'package:stopover_app/styles.dart';
 import 'package:stopover_app/widget/poi_list.dart';
 
 class CategoryDetailPage extends Scaffold {
   CategoryDetailPage(Category category)
       : super(
           appBar: AppBar(
-            title: Text(category.name),
+            title: Text(
+              category.name.toUpperCase(),
+              style: Styles.navBarTitle,
+            ),
           ),
           body: StreamBuilder<List<Poi>>(
             stream: poiBloc.allPois,
